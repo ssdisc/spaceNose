1a3d91a99c9a90bdf46c1993f4026ace05d65988
deeperthought <deeperthought@noreply.gitcode.com>
2025-10-31 17:01:08 +0800

    feat(uart): 降低串口波特率至9600
    将串口通信的波特率从15200调整为9600，以适配新的通信需求。
    同时更新了platformio.ini中的monitor_speed配置和README.md中的说明文档。

--- src/main.c
+++ src/main.c
@@ -197,7 +197,7 @@ static void MX_USART1_UART_Init(void)
     __HAL_RCC_USART1_CLK_ENABLE();
 
     huart1.Instance = USART1;
-    huart1.Init.BaudRate = 115200;
+    huart1.Init.BaudRate = 9600;
     huart1.Init.WordLength = UART_WORDLENGTH_8B;
     huart1.Init.StopBits = UART_STOPBITS_1;
     huart1.Init.Parity = UART_PARITY_NONE;
